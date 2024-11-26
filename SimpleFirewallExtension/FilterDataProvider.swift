9c04aae59f8558ffdd6aabb68391830a3e3f878b
banv <banv-dev@users.noreply.github.com>
2024-11-26 09:01:09 +0700

    try to filte http request

--- SimpleFirewallExtension/FilterDataProvider.swift
+++ SimpleFirewallExtension/FilterDataProvider.swift
@@ -14,37 +14,25 @@ import os.log
  */
 class FilterDataProvider: NEFilterDataProvider {
 
-    // MARK: Properties
-
-    // The TCP port which the filter is interested in.
-	static let localPort = "8888"
 
     // MARK: NEFilterDataProvider
 
     override func startFilter(completionHandler: @escaping (Error?) -> Void) {
-
-        // Filter incoming TCP connections on port 8888
-        let filterRules = ["0.0.0.0", "::"].map { address -> NEFilterRule in
-            let localNetwork = NWHostEndpoint(hostname: address, port: FilterDataProvider.localPort)
-            let inboundNetworkRule = NENetworkRule(remoteNetwork: nil,
-                                                   remotePrefix: 0,
-                                                   localNetwork: localNetwork,
-                                                   localPrefix: 0,
-                                                   protocol: .TCP,
-                                                   direction: .inbound)
-            return NEFilterRule(networkRule: inboundNetworkRule, action: .filterData)
-        }
-
-        // Allow all flows that do not match the filter rules.
-        let filterSettings = NEFilterSettings(rules: filterRules, defaultAction: .allow)
-
-        apply(filterSettings) { error in
-            if let applyError = error {
-                os_log("Failed to apply filter settings: %@", applyError.localizedDescription)
-            }
-            completionHandler(error)
-        }
-    }
+           let filterSettings = NEFilterSettings(rules: [NEFilterRule(networkRule: NENetworkRule(
+               remoteNetwork: nil,
+               remotePrefix: 0,
+               localNetwork: nil,
+               localPrefix: 0,
+               protocol: .any,
+               direction: .outbound
+           ), action: .filterData)], defaultAction: .allow)
+           apply(filterSettings) { error in
+               if let applyError = error {
+                   os_log("Failed to apply filter settings: %@", applyError.localizedDescription)
+               }
+               completionHandler(error)
+           }
+       }
     
     override func stopFilter(with reason: NEProviderStopReason, completionHandler: @escaping () -> Void) {
 
@@ -52,30 +40,20 @@ class FilterDataProvider: NEFilterDataProvider {
     }
     
     override func handleNewFlow(_ flow: NEFilterFlow) -> NEFilterNewFlowVerdict {
-
+   
         guard let socketFlow = flow as? NEFilterSocketFlow,
-            let remoteEndpoint = socketFlow.remoteEndpoint as? NWHostEndpoint,
-            let localEndpoint = socketFlow.localEndpoint as? NWHostEndpoint else {
-                return .allow()
-        }
+              let remoteEndpoint = socketFlow.remoteEndpoint,
+              let url = socketFlow.url
+                else {
+                    return .allow()
+                }
+        
+        NSLog("socket url \(url)")
+        NSLog("socket remote \(remoteEndpoint)")
 
-        os_log("Got a new flow with local endpoint %@, remote endpoint %@", localEndpoint, remoteEndpoint)
+        
 
-        let flowInfo = [
-            FlowInfoKey.localPort.rawValue: localEndpoint.port,
-            FlowInfoKey.remoteAddress.rawValue: remoteEndpoint.hostname
-        ]
-
-        // Ask the app to prompt the user
-        let prompted = IPCConnection.shared.promptUser(aboutFlow: flowInfo) { allow in
-            let userVerdict: NEFilterNewFlowVerdict = allow ? .allow() : .drop()
-            self.resumeFlow(flow, with: userVerdict)
-        }
-
-        guard prompted else {
-            return .allow()
-        }
-
-        return .pause()
+        
+        return .allow()
     }
 }
